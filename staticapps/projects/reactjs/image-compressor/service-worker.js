f0d5250d26cc4361e82154081c5fad632660121c
Oswin_Jerome <oswinjeromej@gmail.com>
2019-11-25 13:16:28 +0530

    dfd

--- staticapps/projects/reactjs/image-compressor/service-worker.js
+++ staticapps/projects/reactjs/image-compressor/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2d2a061b9fae60a28b4b8f34d2752825.js"
+  "./precache-manifest.c44c37e90b27dcd3967b796f99855739.js"
 );
 
 self.addEventListener('message', (event) => {
