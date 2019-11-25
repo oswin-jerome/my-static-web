5857084686bc42b49894ac9c7e801d584665c485
Oswin_Jerome <oswinjeromej@gmail.com>
2019-11-25 12:49:07 +0530

    new

---
+++ staticapps/projects/reactjs/image-compressor/precache-manifest.6c38ec31437fb4961e51e724448ccd37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eebf6edc20c8e94e1c76d10e49e8faab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ccf1213c74df2e7d8af",
+    "url": "/static/css/main.cd9a8a5a.chunk.css"
+  },
+  {
+    "revision": "f625de8e0e77f253d70a",
+    "url": "/static/js/2.c6802aaf.chunk.js"
+  },
+  {
+    "revision": "6ccf1213c74df2e7d8af",
+    "url": "/static/js/main.67f5c036.chunk.js"
+  },
+  {
+    "revision": "1a33693fdae67814cc50",
+    "url": "/static/js/runtime-main.0963ae92.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
